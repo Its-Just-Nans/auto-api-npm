a23fe5ab9ad8884e83b57d2ccec12ac4f07b3cf3
n4n5 <its.just.n4n5@gmail.com>
2021-12-06 09:24:50 +0100

    [fix] change url

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const axios = require('axios');
 const packageName = "csv-to-custom-json";
 
 const job = async () => {
-    const rep = await axios.get(`https://api.npms.io/v2/package/${packageName}`);
-    rep.data.collected.npm.downloads.map(entry=>{
-        console.log(JSON.stringify(entry));
-    });
+    // const rep = await axios.get(`https://api.npms.io/v2/package/${packageName}`);
+    // rep.data.collected.npm.downloads.map(entry => {
+    //     console.log(JSON.stringify(entry));
+    // });
+    const repLastWeek = await axios.get(`https://api.npmjs.org/downloads/point/last-week/${packageName}`);
+    console.log(`Last week downloads : ${repLastWeek.data.downloads}`);
 }
 
 job();
